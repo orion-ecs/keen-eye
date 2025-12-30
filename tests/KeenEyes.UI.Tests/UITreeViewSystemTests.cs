b5d29b984c88016d7ffcabf5737ccc62371dbe29
Tyler Coles <tyler@tyev.co>
2025-12-30 01:49:07 -0500

    Add UITreeViewSystem edge case tests for coverage

--- tests/KeenEyes.UI.Tests/UITreeViewSystemTests.cs
+++ tests/KeenEyes.UI.Tests/UITreeViewSystemTests.cs
@@ -332,6 +332,387 @@ public class UITreeViewSystemTests
 
     #endregion
 
+    #region Edge Case Tests
+
+    [Fact]
+    public void TreeNodeArrow_WithNestedParent_FindsTreeNode()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        // Create child container
+        var childContainer = world.Spawn()
+            .With(new UIElement { Visible = false })
+            .Build();
+
+        // Create node
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Parent")
+            {
+                HasChildren = true,
+                IsExpanded = false,
+                ChildContainer = childContainer
+            })
+            .With(new UIStyle())
+            .Build();
+
+        // Create intermediate container
+        var arrowContainer = world.Spawn()
+            .With(UIElement.Default)
+            .Build();
+        world.SetParent(arrowContainer, node);
+
+        // Create arrow nested in container
+        var arrow = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UIText { Content = "▶" })
+            .With(new UITreeNodeExpandArrowTag())
+            .With(UIInteractable.Clickable())
+            .Build();
+        world.SetParent(arrow, arrowContainer);
+        world.SetParent(childContainer, node);
+        world.SetParent(node, treeView);
+
+        // Click should still find node through parent chain
+        SimulateClick(world, arrow);
+        system.Update(0);
+
+        ref readonly var nodeData = ref world.Get<UITreeNode>(node);
+        Assert.True(nodeData.IsExpanded);
+    }
+
+    [Fact]
+    public void TreeNodeArrow_WithoutParentTreeNode_DoesNothing()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create orphan arrow (no tree node parent)
+        var arrow = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UIText { Content = "▶" })
+            .With(new UITreeNodeExpandArrowTag())
+            .With(UIInteractable.Clickable())
+            .Build();
+
+        // Should not crash
+        SimulateClick(world, arrow);
+        system.Update(0);
+    }
+
+    [Fact]
+    public void TreeNode_ChildContainerWithoutUIElement_DoesNotCrash()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        // Create child container WITHOUT UIElement
+        var childContainer = world.Spawn()
+            .Build();
+
+        // Create arrow
+        var arrow = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UIText { Content = "▶" })
+            .With(new UITreeNodeExpandArrowTag())
+            .With(UIInteractable.Clickable())
+            .Build();
+
+        // Create node
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node")
+            {
+                HasChildren = true,
+                IsExpanded = false,
+                ChildContainer = childContainer
+            })
+            .With(new UIStyle())
+            .Build();
+        world.SetParent(arrow, node);
+        world.SetParent(node, treeView);
+
+        // Should not crash
+        SimulateClick(world, arrow);
+        system.Update(0);
+
+        ref readonly var nodeData = ref world.Get<UITreeNode>(node);
+        Assert.True(nodeData.IsExpanded);
+    }
+
+    [Fact]
+    public void TreeNode_InvalidTreeView_DoesNotFireEvents()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create arrow without proper tree view association
+        var arrow = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UIText { Content = "▶" })
+            .With(new UITreeNodeExpandArrowTag())
+            .With(UIInteractable.Clickable())
+            .Build();
+
+        // Create node with NULL tree view
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(Entity.Null, Entity.Null, 0, "Node")
+            {
+                HasChildren = true,
+                IsExpanded = false
+            })
+            .With(new UIStyle())
+            .Build();
+        world.SetParent(arrow, node);
+
+        int expandEvents = 0;
+        world.Subscribe<UITreeNodeExpandedEvent>(e => expandEvents++);
+
+        SimulateClick(world, arrow);
+        system.Update(0);
+
+        Assert.Equal(0, expandEvents);
+    }
+
+    [Fact]
+    public void TreeNode_ClickWithInvalidTreeView_IsIgnored()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create node with invalid tree view reference
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(Entity.Null, Entity.Null, 0, "Node"))
+            .With(UIInteractable.Clickable())
+            .With(new UIStyle())
+            .Build();
+
+        int selectEvents = 0;
+        world.Subscribe<UITreeNodeSelectedEvent>(e => selectEvents++);
+
+        SimulateClick(world, node);
+        system.Update(0);
+
+        Assert.Equal(0, selectEvents);
+    }
+
+    [Fact]
+    public void TreeNode_ClickWithDeadTreeView_IsIgnored()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create and destroy tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node"))
+            .With(UIInteractable.Clickable())
+            .With(new UIStyle())
+            .Build();
+
+        world.Despawn(treeView);
+
+        int selectEvents = 0;
+        world.Subscribe<UITreeNodeSelectedEvent>(e => selectEvents++);
+
+        SimulateClick(world, node);
+        system.Update(0);
+
+        Assert.Equal(0, selectEvents);
+    }
+
+    [Fact]
+    public void TreeNode_DeselectPreviousWithoutUITreeNode_DoesNotCrash()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        var (treeView, nodes, arrows) = CreateTreeView(world, hasChildren: false);
+
+        // Select first node
+        SimulateClick(world, nodes[0]);
+        system.Update(0);
+
+        // Remove UITreeNode from first node (simulate edge case)
+        world.Remove<UITreeNode>(nodes[0]);
+
+        // Try to select second node - should not crash
+        SimulateClick(world, nodes[1]);
+        system.Update(0);
+    }
+
+    [Fact]
+    public void TreeNode_SelectSameNode_DoesNotDeselect()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        var (treeView, nodes, arrows) = CreateTreeView(world, hasChildren: false);
+
+        // Select first node
+        SimulateClick(world, nodes[0]);
+        system.Update(0);
+
+        // Click same node again
+        SimulateClick(world, nodes[0]);
+        system.Update(0);
+
+        ref readonly var node = ref world.Get<UITreeNode>(nodes[0]);
+        Assert.True(node.IsSelected);
+    }
+
+    [Fact]
+    public void TreeNodeArrow_WithoutUIText_DoesNotCrash()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        // Create child container
+        var childContainer = world.Spawn()
+            .With(new UIElement { Visible = false })
+            .Build();
+
+        // Create arrow WITHOUT UIText
+        var arrow = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNodeExpandArrowTag())
+            .With(UIInteractable.Clickable())
+            // No UIText!
+            .Build();
+
+        // Create node
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node")
+            {
+                HasChildren = true,
+                IsExpanded = false,
+                ChildContainer = childContainer
+            })
+            .With(new UIStyle())
+            .Build();
+        world.SetParent(arrow, node);
+        world.SetParent(childContainer, node);
+        world.SetParent(node, treeView);
+
+        // Should not crash
+        SimulateClick(world, arrow);
+        system.Update(0);
+
+        ref readonly var nodeData = ref world.Get<UITreeNode>(node);
+        Assert.True(nodeData.IsExpanded);
+    }
+
+    [Fact]
+    public void TreeNode_SelectWithoutUIStyle_DoesNotCrash()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        // Create node WITHOUT UIStyle
+        var node = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node"))
+            .With(UIInteractable.Clickable())
+            // No UIStyle!
+            .Build();
+        world.SetParent(node, treeView);
+
+        // Should not crash
+        SimulateClick(world, node);
+        system.Update(0);
+
+        ref readonly var nodeData = ref world.Get<UITreeNode>(node);
+        Assert.True(nodeData.IsSelected);
+    }
+
+    [Fact]
+    public void TreeNode_DeselectWithoutUIStyle_DoesNotCrash()
+    {
+        using var world = new World();
+        var system = new UITreeViewSystem();
+        world.AddSystem(system);
+
+        // Create tree view
+        var treeView = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeView())
+            .Build();
+
+        // Create first node without UIStyle
+        var node1 = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node1"))
+            .With(UIInteractable.Clickable())
+            .Build();
+        world.SetParent(node1, treeView);
+
+        // Create second node with UIStyle
+        var node2 = world.Spawn()
+            .With(UIElement.Default)
+            .With(new UITreeNode(treeView, Entity.Null, 0, "Node2"))
+            .With(UIInteractable.Clickable())
+            .With(new UIStyle())
+            .Build();
+        world.SetParent(node2, treeView);
+
+        // Select first node
+        SimulateClick(world, node1);
+        system.Update(0);
+
+        // Select second node (deselects first - but first has no UIStyle)
+        SimulateClick(world, node2);
+        system.Update(0);
+
+        // Should not crash and second should be selected
+        ref readonly var node2Data = ref world.Get<UITreeNode>(node2);
+        Assert.True(node2Data.IsSelected);
+    }
+
+    #endregion
+
     #region Helper Methods
 
     private static (Entity TreeView, Entity[] Nodes, Entity[] Arrows) CreateTreeView(
